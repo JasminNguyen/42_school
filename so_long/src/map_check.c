239e48099c48b177252723272b956dc003173b45
Jasmin Ngoc Han Nguyen <jasnguye@c1b3c1.42berlin.de>
2024-02-06 16:42:14 +0100

    Added invalid maps, and more map checks

--- so_long/src/map_check.c
+++ so_long/src/map_check.c
@@ -6,7 +6,7 @@
 /*   By: jasnguye <jasnguye@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 12:50:27 by jasnguye          #+#    #+#             */
-/*   Updated: 2024/02/06 14:27:03 by jasnguye         ###   ########.fr       */
+/*   Updated: 2024/02/06 16:37:15 by jasnguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int is_rectangular(t_game *game)
     {
         if (game->map[i][0] != '\n')
         {
-            return 0; // Not rectangular
+            return (0); // Not rectangular
         }
         i++;
     }
@@ -68,7 +68,7 @@ int is_valid_char(t_game *game)
 			
 			if(!(game->map[i][j] == '0' || game->map[i][j] == '1' || game->map[i][j] == 'P' || game->map[i][j] == 'E' || game->map[i][j] == 'C'))
 			{
-				return (0);
+				return (0); //not valid
 			}
 			j++;
 		}
@@ -77,16 +77,76 @@ int is_valid_char(t_game *game)
 	return(1);
 }
 
+int is_surrounded_by_walls(t_game *game)
+{
+	
+	int j = 0;
+	int i = 0;
+	while (j < game->map_width)
+	{
+		if((game->map[0][j] != '1') || (game->map[game->map_height - 1][j] != '1'))
+		{
+			return(0);
+		}
+		j++;
+	}
+	while (i < game->map_height)
+	{
+		if(game->map[i][game->map_width -1] != '1' || game->map[i][0] != '1')
+		{
+			return(0);
+		}
+		i++;
+	}
 
+	return(1);
+}
 
+int element_check(t_game *game)
+{
+	int i = 0;
+	int j = 0;
+	int collectible = 0;
+	int exit = 0;
+	int player = 0;
+	
+	while(i < game->map_height)
+	{
+		j = 0;
+		while (j < game->map_width)
+		{
+			if(game->map[i][j] == 'C' )
+			{
+				collectible++;
+			}
+			else if(game->map[i][j] == 'E' )
+			{
+				exit++;
+			}
+			else if(game->map[i][j] == 'P')
+			{
+				player++;
+			}
+			j++;
+		}
+		i++;
+		
+	}
+	
+	if(exit == 1 && collectible >= 1 && player == 1)
+	{
+		return (1);
+	}
+	return(0);
+}
 
 
 int map_check(t_game *game)
 {
 	
-	if(!(is_rectangular(game)) || !(is_valid_char(game)))
+	if(!(is_rectangular(game)) || !(is_valid_char(game)) || !(is_surrounded_by_walls(game))  || !(element_check(game)) )
 	{
-		ft_printf("Invalid map!");
+		ft_printf("Error\nInvalid map!");
 		return(0);
 	}
 	return(1);
