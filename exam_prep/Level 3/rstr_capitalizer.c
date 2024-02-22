48c9d6a4967ea1718d0e3bd291b79bcfce4ae15a
JasminNguyen <jasminnguyen001@web.de>
2024-02-22 20:25:40 +0100

    modified rstr_capitalizer.c, is working now

--- exam_prep/Level 3/rstr_capitalizer.c
+++ exam_prep/Level 3/rstr_capitalizer.c
@@ -1,59 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rstr_capitalizer.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jasnguye <jasnguye@student.42berlin.d      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/15 15:14:09 by jasnguye          #+#    #+#             */
-/*   Updated: 2024/01/15 16:01:48 by jasnguye         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-int ft_strlen(char *str)
+void    rstr_capitalizer(char *str)
 {
-	int i = 0;
-	while(str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-void ft_transform_char(char *str, char c, int index)
-{
-	if ((index == (ft_strlen(str) - 1)) || ((str[index + 1] == ' ' || str[index + 1] == '\t') && (str[index] >= 'a' && str[index] <= 'z')))
-	{
+    int i = 0;
 
-		c = c - 32;
-	}
-	write(1, &c, 1);
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + 32;
+        }
+        
+        if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\0'))
+        {
+            str[i] = str[i] - 32;
+        }
+        write(1, &str[i], 1);
+        i++;
+    }
 }
-int main(int argc, char *argv[])
-{
-	if(argc > 1)
-	{
-		int i = 0;
-		int j = 1;
-		while(j < argc)
-		{	
-			i = 0;
-			while(argv[j][i] != '\0')
-			{	
-				
-				if(argv[j][i] >= 'A' && argv[j][i] <= 'Z')
-				{
-					argv[j][i] = argv[j][i] + 32;
-				}
-				ft_transform_char(argv[j], argv[j][i], i);
-				i++;
-			}
-			j++;
-			write (1, "\n", 1);
-		}
 
-	}
-	else 
-	write(1, "\n", 2);
+int main(int argc, char **argv)
+{
+    int i;
+
+    if (argc == 1)
+        write(1, "\n", 1);
+    else
+    {
+        i = 1;
+        while (i < argc)
+        {
+            rstr_capitalizer(argv[i]);
+            write(1, "\n", 1);
+            i++;
+        }
+    }
 }
