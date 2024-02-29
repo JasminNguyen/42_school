bec99f7ddc3bf4295bb4677339f6e376ad0d13b7
JasminNguyen <jasminnguyen001@web.de>
2024-02-29 15:59:02 +0100

    added expand_str.c to Level 3

---
+++ exam_prep/Level 3/expand_str.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+//just like epur_str only with 3 spaces in between 
+int main(int argc, char *argv[])
+{
+    if(argc == 2)
+    {
+        int i = 0;
+        int flag = 0;
+        while(argv[1][i] == ' ' || argv[1][i] == '\t')
+        {
+            i++;
+        }
+        while(argv[1][i] != '\0')
+        {
+            if(argv[1][i] == ' '  || argv[1][i] == '\t' )
+            {
+               flag = 1;
+            }
+            if(argv[1][i] != ' ' && argv[1][i] != '\t')
+            {
+                if(flag)
+                {
+                    write(1, "   ", 3);
+                    flag = 0;
+                }
+                write(1, &argv[1][i], 1);
+            } 
+          
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+}
