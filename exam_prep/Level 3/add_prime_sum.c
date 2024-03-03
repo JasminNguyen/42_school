e30f2e0f4bc5622fb8ec2a5ddce6946ca0d27e91
JasminNguyen <132588241+JasminNguyen@users.noreply.github.com>
2024-03-03 17:31:10 +0100

    Update add_prime_sum.c

--- exam_prep/Level 3/add_prime_sum.c
+++ exam_prep/Level 3/add_prime_sum.c
@@ -15,99 +15,93 @@
 
 int ft_atoi(char *str)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-	{
-		i++;
-	}
-	while(str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '+')
-		{
-			sign *= -1;
-		}
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + str[i] - '0';
-		i++;
-	}
-	return(sign * result);
+    int i = 0;
+    int sign = 1;
+    int result = 0;
+    while(str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+    {
+        i++;
+    }
+    while (str[i] == '-' || str[i] == '+')
+    {
+        if(str[i] == '-')
+        {
+            sign = -1;
+        }
+        i++;
+    }
+    while(str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + str[i] - '0';
+        i++;
+    }
+    return (result * sign);
 }
 
-
-int is_prime(int nbr)
+int prime(int nbr)
 {
-	int i = 2;
-
-	if (nbr <= 1)
-	{
-		return (0);
-	}
-	while (i < nbr)
-	{
-		if(nbr % i == 0)
-		{
-			return (0);
-		}
-		i++;
-	}
-	return (1);
+    int i = 2;
+    if(nbr == 1)
+    {
+        return (0);
+    }
+    while(i < nbr)
+    {
+        if(nbr % i == 0)
+        {
+            return (0);
+        }
+        i++;
+    }
+    return (1);
 
 }
-
 void ft_putchar(char c)
 {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
-
 void ft_putnbr(int nbr)
 {
-	if (nbr == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-	}
-	else 
-	{
-		if(nbr < 0)
-		{
-			write(1, "-", 1);
-			nbr = -nbr;
-		}
-		if(nbr > 9)
-		{
-			ft_putnbr(nbr / 10);
-			nbr = nbr % 10;
-		}
-		ft_putchar(nbr + '0');
-	}
+    if(nbr == -2147483648)
+    {
+        write(1, "-2147483648", 11);
+    }
+    else
+    {
+        if(nbr < 0)
+        {
+            write(1, "-", 1);
+            nbr = -nbr;
+        }
+        if(nbr > 9)
+        {
+            ft_putnbr(nbr / 10);
+            nbr = nbr % 10;
+        }
+        ft_putchar(nbr + '0');
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	if(argc == 2 || argv[1] < 0)
-	{	
-		int result = 0;
-		int i = 0;
-		int max_value = ft_atoi(argv[1]);
-
-		while (i <= max_value)
-		{
-			if (is_prime(i) == 1)
-			{
-				result += i;
-			}
-			i++;
-		}
-		ft_putnbr(result);
-	}
-	else 
-	{
-		write(1, "0", 1);
-	}
-	write(1, "\n", 1);
+    if(argc == 2) 
+    {
+        int result = 0;
+        int max_value = ft_atoi(argv[1]);
+        int i = 0;
+        while(i <= max_value)
+        {
+            if(prime(i) == 1)
+            {
+                result = result + i;
+            }
+            i++;
+        }
+        ft_putnbr(result);
+        write(1, "\n", 1);
+    }
+    else
+    {
+        write(1, "0\n", 2);
+    }
 }
-
