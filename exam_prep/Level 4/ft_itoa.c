36c04cac0c1b5776d3cb4f790b6c6584903b05bb
JasminNguyen <132588241+JasminNguyen@users.noreply.github.com>
2024-03-03 17:51:14 +0100

    Update ft_itoa.c

--- exam_prep/Level 4/ft_itoa.c
+++ exam_prep/Level 4/ft_itoa.c
@@ -3,38 +3,38 @@
 
 char	*ft_itoa(int nbr)
 {
-    int copy = nbr; 
-    int len = 0; 
-    char *result; 
-    int i = 0; 
-    if(nbr == -2147483648)
+    int copy = nbr; //put nbr in copy variable
+    int len = 0; //important for malloc
+    char *result; // string to be returned 
+    int i = 0; //to iterate through result
+    if(nbr == -2147483648) 
     {
-        return("-2147483648\0"); 
+        return("-2147483648\0"); //edge case 
     }
-    if(nbr <= 0)
+    if(nbr <= 0) // <= to get len if nbr is smaller or equal 0
     {
         len++; 
     }
-    while(copy != 0)
+    while(copy != 0) //get len
     {
         copy /= 10; 
         len++; 
     }
-    result = (char *)malloc(sizeof(char) * len + 1);  
-   result[len] = '\0'; 
-    if(nbr == 0)
+    result = (char *)malloc(sizeof(char) * len + 1);  //allocate memory
+   result[len] = '\0'; //put null-terminator
+    if(nbr == 0) 
     {
-        result[i] = '0'; 
-        return(result); 
+        result[i] = '0'; //put 0
+        return(result); // return immediately
     }
     if(nbr < 0)
     {
-        result[i] = '-'; 
-        nbr = -nbr; 
+        result[i] = '-'; //if nbr is negative
+        nbr = -nbr; // make it positive
     }
-    while(nbr)
+    while(nbr) //while nbr exists
     {
-        result[--len] = (nbr % 10) + '0'; 
+        result[--len] = (nbr % 10) + '0'; //start filling up the string from the end and turning it into ascii -> [--len] to get the actual last position (indexing) 
         nbr = nbr / 10; 
     }
    
