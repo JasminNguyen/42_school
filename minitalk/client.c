ab97a7c71f0e763b2cbda4adc3759e088cf65f15
Jasmin Ngoc Han Nguyen <jasminnguyen001@web.de>
2024-03-15 18:24:09 +0100

    last version of Friday

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: jasnguye <jasnguye@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 16:37:14 by jasnguye          #+#    #+#             */
-/*   Updated: 2024/03/15 16:31:50 by jasnguye         ###   ########.fr       */
+/*   Updated: 2024/03/15 18:22:30 by jasnguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	send_message(int pid, char *message)
 			{
 				kill(pid, SIGUSR2);
 			}
-			usleep(100); //Make the process sleep for 100 microseconds. This is done to ensure that the server has enough time to handle the signal before the next one is sent. 
+			usleep(1200); //Make the process sleep for 100 microseconds. This is done to ensure that the server has enough time to handle the signal before the next one is sent. 
 			bit_position++;
 		}
 		i++;
