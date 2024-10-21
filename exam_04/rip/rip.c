81dd6098f7a9c0f7573809ae7a4eaea514d31580
Jasmin Ngoc Han Nguyen <jasminnguyen001@web.de>
2024-10-21 19:57:20 +0200

    improved rip, edge cases not working

--- exam_04/rip/rip.c
+++ exam_04/rip/rip.c
@@ -26,18 +26,10 @@ _ ( ) _ ( ) _ $ */
 
 
 #include <stdio.h> 
-
-int ft_strlen(char *string)
-{
-	int i = 0;
-	while(string[i] != '\0')
-	{
-		i++;
-	}
-	return(i);
-}
+#include <string.h>
 
 
+/* my first approach: 
 void remove_function(char *string, char *result_string, int left, int right, int to_remove)
 {
 	int i = 0;
@@ -82,10 +74,104 @@ void remove_function(char *string, char *result_string, int left, int right, int
 	}
 			printf("result string is: %s\n", result_string);
 	
+} */
+
+
+
+int ft_strlen(char *string)
+{
+	int i = 0;
+	while(string[i] != '\0')
+	{
+		i++;
+	}
+	return(i);
+}
+/* For the string "((()))", as we traverse the string, balance will go up initially due to the opening parentheses (, 
+but it will later return to zero as the closing parentheses ) are processed.
+Thus, a positive balance is not inherently invalid during traversal, but it is necessary to ensure that by the 
+end of the string, the balance is exactly zero. This means the number of opening and closing parentheses matches. */
+int valid_solution(char *str)
+{
+	int i = 0;
+	int balance = 0;
+	while(str[i] != '\0')
+	{
+		if(str[i] == '(')
+		{
+			balance++;
+		}
+		else if(str[i] == ')')
+		{
+			balance--;
+		}
+		 if (balance < 0)
+        {
+            return -1;  // Invalid solution
+        }
+		i++;
+	}
+	if(balance == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return(-1);
+	}
 }
 
+void remove_function(char *string, char *result_string, int i, int to_remove, int removed, int left, int right)
+{
+    // Base case: if we have traversed the entire string or removed enough parentheses
+    if (string[i] == '\0')
+    {
+        if (removed == to_remove && valid_solution(result_string) == 1)
+        {
+            result_string[i] = '\0'; // Null-terminate the result string
+            printf("result string is:: %s\n", result_string); // Print one possible solution
+			puts(result_string);
+        }
+        return;
+    }
+	
+	//exclude
+  	if(string[i] == '(' && left > right && to_remove > removed)
+	{
+		result_string[i] = '_';
+		remove_function(string, result_string, i + 1, to_remove, removed +1, left -1, right);
+	}
+	else if(string[i] == ')' && right > left && to_remove > removed)
+	{
+		result_string[i] = '_';
+		remove_function(string, result_string, i + 1, to_remove, removed +1, left, right -1);
+	}
+
+	//include 
+	result_string[i] = string[i];
+	if(string[i] == '(')
+	{
+		remove_function(string, result_string, i + 1, to_remove, removed, left, right);
+	}
+	else if(string[i] == ')')
+	{
+		remove_function(string, result_string, i + 1, to_remove, removed, left, right);
+	}
+	else 
+	{
+		remove_function(string, result_string, i + 1, to_remove, removed, left, right);
+	}
+		
+		
+		
+}
+
+
+
 /*
-the system automatically allocates memory for argv[1] which I don't have to free, but for any other string that I create I need to either allocate memory dynamically or define the size otherwise I don't have a valid memory space for it and it will segfault*/
+the system automatically allocates memory for argv[1] which I don't have to free,
+but for any other string that I create I need to either allocate memory dynamically 
+or define the size otherwise I don't have a valid memory space for it and it will segfault*/
 int main(int argc, char *argv[])
 {
 	(void)argc;
@@ -95,33 +181,48 @@ int main(int argc, char *argv[])
 	printf("len is: %d\n", len);
 	
 	int i = 0;
-	int left = 0;
-	int right = 0;
+	int invalid_left = 0;
+	int invalid_right = 0;
 	int to_remove = 0;
+	int balance = 0;
+
 	while(string[i] != '\0')
 	{
 		if(string[i] == '(')
 		{
-			left++;
+			balance++;
 		}
-		else if (string[i] == ')')
+		else if(string[i] == ')')
 		{
-			right++;
+			balance--;
+		}
+		if(balance < 0)
+		{
+			invalid_right++;
+		}
+		else if(balance > 0)
+		{
+			invalid_left++;
 		}
 		i++;
 	}
-	if(left > right)
+	printf("invalid lefts: %d\n", invalid_left);
+	printf("invalid rights: %d\n", invalid_right);
+	printf("to_remove is: %d\n", to_remove);
+
+	//check for same amount of left and right parenthesis and no open parethesis (correct nesting)
+	if(to_remove == 0 && valid_solution(string) == 1)
 	{
-		to_remove = left - right;
+		printf("no changes to string: %s\n", string);
+		puts(string);
 	}
 	else
 	{
-		to_remove = right - left;
+		remove_function(string, result_string, 0, to_remove, 0, left, right);
 	}
-	printf("left is: %d\n", left);
-	printf("right is: %d\n", right);
-	printf("to_remove is: %d\n", to_remove);
 	
-	remove_function(string, result_string, left, right, to_remove);
 	
+
 }
+
+
