91d82932424964f188ab53abb350181e969887e6
JasminNguyen <jasminnguyen001@web.de>
2024-11-20 13:31:39 +0100

    finished n_queens

--- exam_04/n_queens/n_queens.c
+++ exam_04/n_queens/n_queens.c
@@ -1,7 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+int find_collisions(int *return_array, int input_len, int start_y, int end_y, int direction)
+{
+    //printf("return_array: ")
+    int current_x = 0;
+    int current_y = start_y;
+    while(current_x != input_len -1 && current_y != end_y - 1)
+    { 
+        current_x++;
+        current_y += direction;
+        if(current_y == return_array[current_x])
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
 void check_permutation(int *return_array, int input_len)
 {
+    int x = 0;
+    while(x < input_len)
+    {
+        int start_y = return_array[x];
+        int end_y = input_len;
+        int direction = 1;
+        
+        if(find_collisions(return_array + x , input_len - x, start_y, end_y, direction) != 0)
+        {
+            return ;
+        }
+        end_y = -1;
+        direction = -1;
+        if(find_collisions(return_array + x, input_len - x , start_y, end_y, direction) != 0)
+        {
+            return ;
+        }        
+        x++;
+
+    }
     int i = 0;
     while(i < input_len)
     {
@@ -56,6 +93,7 @@ void build_permutations(int *return_array, int *array, int return_array_index, i
 }
 int main(int argc, char *argv[])
 {
+    (void)argc;
     int n = atoi(argv[1]);
     int *array = malloc(sizeof(int) * n);
     if(!array)
