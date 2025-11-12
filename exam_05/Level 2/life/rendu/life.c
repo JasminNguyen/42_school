230fe27d3441cef37d8b047b4e4e77d770e68918
JasminNguyen <jasminnguyen001@web.de>
2025-11-12 15:04:30 +0100

    add initialization of cells

--- exam_05/Level 2/life/rendu/life.c
+++ exam_05/Level 2/life/rendu/life.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
+
+bool within_board(int pos_x, int pos_y, int width, int height)
+{
+	if(pos_x < width && pos_y < height && pos_x >= 0 && pos_y >= 0)
+	{
+		return true;
+	}
+	return false;
+}
 int main(int argc, char *argv[])
 {
 	if(argc != 4)
@@ -16,14 +26,74 @@ int main(int argc, char *argv[])
 		char board[width][height];
 		char next_board[width][height];
 
-		//read input to initialize cells to 0
+		//initialize cells in board to 0
+		//x is a column, y is a row
 		for(int y = 0; y < height; y++)
 		{
 			for(int x = 0; x < width; x++)
 			{
 				board[x][y] = 0;
+				next_board[x][y] = 0;
 			}
 		}
+		//set alive cells to 1 in board
+		char cmd;
+		
+		int pos_x = 0;
+		int pos_y = 0;
+		bool pen_down = false;
+
+		while (read(0, &cmd, 1) > 0)
+		{
+			if(cmd == 'w') //up
+			{
+				pos_y--;
+				if(pen_down && within_board(pos_x, pos_y, width, height))
+				{
+					board[pos_x][pos_y] = 1;
+				}
+			}
+			else if(cmd == 'a') //left
+			{
+				pos_x--;
+				if(pen_down && within_board(pos_x, pos_y, width, height))
+				{
+					board[pos_x][pos_y] = 1;
+				}
+			}
+			else if(cmd == 's') //down
+			{
+				pos_y++;
+				if(pen_down && within_board(pos_x, pos_y, width, height))
+				{
+					board[pos_x][pos_y] = 1;
+				}
+			}
+			else if(cmd == 'd') //right
+			{
+				pos_x++;
+				if(pen_down && within_board(pos_x, pos_y, width, height))
+				{
+					board[pos_x][pos_y] = 1;
+				}
+			}
+			else if(cmd == 'x') //toggle pen
+			{
+				if(!pen_down)
+				{
+					pen_down = true;
+					if(within_board(pos_x, pos_y, width, height))
+					{
+						board[pos_x][pos_y] = 1;
+					}
+				}
+				else
+				{
+					pen_down = false;
+				}
+			}
+		}
+
 		
 	}
 	return 0;
