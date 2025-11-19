c6c477e703f4e192f3a132f75758af67d457f876
Jasmin Ngoc Han Nguyen <jasminnguyen001@web.de>
2025-11-19 17:31:38 +0100

    add bsq solution

--- exam_05/Level 2/bsq/rendu/bsq.c
+++ exam_05/Level 2/bsq/rendu/bsq.c
@@ -1,215 +1,167 @@
-			
+#include <malloc.h>
 #include "bsq.h"
 
-void	free_bsq(t_bsq *bsq)
+int is_valid_header(FILE *stream, t_map *map)
 {
-	if(bsq)
+	if (fscanf(stream, "%d%c%c%c\n", &(map->map_y), &(map->empty), &(map->obst), &(map->full)) != 4) //check header for 4 successfully returned elements
 	{
-		if(bsq->stream && bsq->stream != stdin)
-			fclose(bsq->stream);
-		if(bsq->map)
+		return 0;
+	}
+		
+	if (map->empty == map->obst || map->empty == map->full || map->obst == map->full || map->map_y < 1)
+	{
+		return 0;
+	}
+	// is good header, init the rest
+	map->map_x = UNINITIALIZED;
+	map->sol_side = UNINITIALIZED;
+	map->sol_y = UNINITIALIZED;
+	map->sol_x = UNINITIALIZED;
+	map->arr2d = malloc(sizeof(char*) * map->map_y); //allocate memory for the height of the number of lines in the map (height)
+	if (!(map->arr2d))
+	{
+		fprintf(stdout, "Error: malloc fail");
+		return 0;
+	}
+	return 1;
+}
+
+int square_fits(t_map *map, int pos_y, int pos_x, int side_size)
+{
+	if (pos_y + side_size > map->map_y || pos_x + side_size > map->map_x) //pos + potential side bigger than square? then noo
+		return 0;
+
+	for (int y = pos_y; y < pos_y + side_size; y++) // is there an obstacle in the potential square?? then nooo
+	{
+		for (int x = pos_x; x < pos_x + side_size; x++)
 		{
-			int i = -1;
-			while(bsq->map[++i])
-				free(bsq->map[i]);
-			free(bsq->map);
+			if (map->arr2d[y][x] == map->obst)
+				return 0;
 		}
-		free(bsq);
+	}
+	return 1;
+}
+
+void try_squares_for_coords(t_map *map, int pos_y, int pos_x)
+{
+	int side_size = 1;
+
+	while (square_fits(map, pos_y, pos_x, side_size))
+	{
+		if (side_size > map->sol_side)
+		{
+			map->sol_side = side_size;
+			map->sol_y = pos_y;
+			map->sol_x = pos_x;
+		}
+		side_size++;
 	}
 }
 
-int	ft_strlen(char *str)
+void find_bsq(t_map *map)
 {
-	int i = 0;
-	
-	while(str && str[i])
-		i++;
-	return i;
-}
-
-int	parse_header(t_bsq *bsq)
-{
-	int n = fscanf(bsq->stream, "%i%c%c%c\n", &bsq->rows, &bsq->empty, &bsq->obst, &bsq->full);
-	if(n != 4 || bsq->rows <= 0
-		|| (bsq->empty == bsq->full || bsq->empty == bsq->obst || bsq->full == bsq->obst))
-		return (printf("Invalid map\n"), 1);
-	return (0);
-}
-
-int read_map(t_bsq *bsq)
-{
-	if(parse_header(bsq))
-		return 1;
-	ssize_t nread;
-	size_t n = 0;
-	char *line = NULL;
-	int map_size = 1;
-	while((nread = getline(&line, &n, bsq->stream)) != -1)
+	for (int pos_y = 0; pos_y < map->map_y; pos_y++)
 	{
-		char **tmp = realloc(bsq->map, (map_size + 1) * sizeof(char *));
-		if(!tmp)
+		for (int pos_x = 0; pos_x < map->map_x; pos_x++)
 		{
-			free(line);
-			return (printf("Error: allocation error\n"), 1);
-		}
-		bsq->map = tmp;
-		bsq->map[map_size] = NULL;
-		bsq->map[map_size - 1] = calloc(nread, sizeof(char));
-		if(!bsq->map[map_size - 1])
-		{
-			free(line);
-			return (printf("Error: Allocation failed\n"), 1);
-		}
-		if(line[nread - 1] != '\n')
-		{
-			free(line);
-			return (printf("Error: Invalid map\n"), 1);
-		}
-		for(int i = 0; i < nread- 1; i++)
-		{
-			bsq->map[map_size - 1][i] = line[i];
-		}
-		map_size++;
-		free(line);
-		n = 0;
-		line = NULL;
-	}
-	if(line)
-		free(line);
-	return (0);
-}
-
-int	validate_map(t_bsq *bsq)
-{
-	if(!bsq->map)
-		return (printf("Error: Invalid map\n"), 1);
-	bsq->cols = ft_strlen(bsq->map[0]);
-	if(bsq->cols < 1)
-		return (printf("Error: Invalid map\n"), 1);
-	int i;
-	for(i = 0; bsq->map[i] != NULL; i++)
-	{
-		if(ft_strlen(bsq->map[i]) != bsq->cols)
-			return (printf("Error: Invalid map\n"), 1);
-		for(int j = 0; j < bsq->cols; j++)
-		{
-			if((bsq->map[i][j] != bsq->empty && bsq->map[i][j] != bsq->obst)
-				|| bsq->map[i][j] < 32 || bsq->map[i][j] > 126)
-				return (printf("Error: Invalid map\n"), 1);
+			try_squares_for_coords(map, pos_y, pos_x);
 		}
 	}
-	if(i != bsq->rows)
-		return (printf("Error: Invalid map\n"), 1);
-	return (0);
 }
 
-int 	find_bsq(t_bsq *bsq)
+int is_within_square(t_map *map, int y , int x)
 {
-	int max_y = 0, max_x = 0, max_len = 0;
-	char dp[bsq->rows + 1][bsq->cols + 1];
-	for(int y = 0; y < bsq->rows; y++)
+	return (y < map->sol_y + map->sol_side && x < map->sol_x + map->sol_side && y >= map->sol_y && x >= map->sol_x);
+}
+
+void print_map(t_map *map)
+{
+	for (int y = 0; y < map->map_y; y++)
 	{
-		for(int x = 0; x < bsq->cols; x++)
+		for (int x = 0; x < map->map_x; x++)
 		{
-			if(bsq->map[y][x] == bsq->obst)
-				dp[y][x] = 0;
-			else if(x == 0 || y == 0)
-				dp[y][x] = 1;
+			if (is_within_square(map, y, x))
+				fprintf(stdout, "%c", map->full); //print full
 			else
-			{
-				int min = dp[y - 1][x];
-				min = min < dp[y][x - 1] ? min : dp[y][x -1];
-				min = min < dp[y - 1][x - 1] ? min : dp[y - 1][x - 1];
-				dp[y][x] = min + 1;
-			}
-			if(max_len < dp[y][x])
-			{
-				max_len = dp[y][x];
-				max_y = y;
-				max_x = x;
-			}
+				fprintf(stdout, "%c", map->arr2d[y][x]); //print original character of map
 		}
+		fprintf(stdout, "\n");
 	}
-	if(max_len == 0)
-		return (printf("Error: this contains only obsticles\n"), 1);
-	for(int y = (max_y - max_len) + 1; y <= max_y; y++)
-	{
-		for(int x = (max_x - max_len) + 1; x <= max_x; x++)
-			bsq->map[y][x] = bsq->full;
-	}
-	return (0);
-}	
+}
 
-int	main(int argc, char **argv)
+void parse_map(FILE *stream, t_map *m)
 {
-	t_bsq *bsq = calloc(1, sizeof(t_bsq));
-	if(!bsq)
+	char *line = NULL; //line buffer (constent read)
+	size_t size = 0; // contains the buffer size
+
+	// extract and validate map body
+	for (int y = 0; y < m->map_y; y++) //loop through height
 	{
-		free(bsq);
-		return (printf("Error: allocation failed\n"), 1);
-	}
-	if(argc == 1)
-		bsq->stream = stdin;
-	else if(argc == 2)
-	{
-		bsq->stream = fopen(argv[1], "r");
-		if(!bsq->stream)
+		int nread = getline(&line, &size, stream);
+		if (m->map_x == UNINITIALIZED) // find width of the map (x)
+			m->map_x = nread - 1;  // (nread -1) bc '\n' retained
+		else if (nread == -1 || (nread - 1) != m->map_x || line[nread - 1] != '\n')
 		{
-			free(bsq);
-			return (printf("Error: could not open the file\n"), 1);
+			fprintf(stdout, "Error: invalid map\n");
+			return;
 		}
+		m->arr2d[y] = malloc(sizeof(char) * m->map_x); // allocate memory for width of the map (x)
+		if (!(m->arr2d[y]))
+		{
+			fprintf(stdout, "Error: malloc fail\n");
+			return;
+		}
+		for (int x = 0; x < m->map_x; x++) //loop through width 
+		{
+			if (!(line[x] == m->empty || line[x] == m->obst )) // check that we only have empties or obstacles
+			{
+				fprintf(stdout, "Error: invalid map\n");
+				return;
+			}
+			m->arr2d[y][x] = line[x]; //populate one line and put it into map
+		}
+		free(line); //free line
+		line = NULL; // set it to NULL so getline does not assume that we want to reuse the line (which is a buffer)
+		size = 0; // set size to 0
 	}
+	if (getline(&line, &size, stream) != -1)
+	{
+		fprintf(stdout, "Error: invalid map\n");
+		return;
+	}
+
+
+	find_bsq(m);
+	print_map(m);
+}
+
+void check_file(FILE* stream)
+{
+	t_map map;
+
+	if (is_valid_header(stream, &map)) //check for valid header
+		parse_map(stream, &map); // parse map
 	else
-	{
-		free(bsq);
-		return (printf("Error: too many arguments\n"), 1);
-	}
-	if(read_map(bsq) || validate_map(bsq) || find_bsq(bsq))
-	{
-		free_bsq(bsq);
-		return 1;
-	}
-	for(int i = 0; i < bsq->rows; i++)
-	{
-		printf("%s\n", bsq->map[i]);
-	}
-	return (0);
+		fprintf(stdout, "Error: invalid map\n");
 }
 
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+int main(int argc, char **argv)
+{
+	FILE *stream = NULL;
 
-		
-		
-		
-		
-		
-		
-		
+	if (argc > 2) //check for too many arguments
+	{
+		fprintf(stdout, "Error: too many arguments\n");
+		return 1;
+	}
+	else if (argc == 2) // if two arguments -> open file in read mode
+		stream = fopen(argv[1], "r");
+	else
+		stream = stdin; // if one argument -> read from stdin
 	
+	if (!stream)
+		fprintf(stdout, "Error: fopen fail\n"); //stream fail
+	else
+		check_file(stream); // if everything correct -> parse file
+}
--- exam_05/Level 2/bsq/rendu/bsq.h
+++ exam_05/Level 2/bsq/rendu/bsq.h
@@ -1,22 +1,20 @@
 #ifndef BSQ_H
-#define BSQ_H
+# define BSQ_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+# define UNINITIALIZED -1
 
-typedef struct s_bsq
-{
-	int rows;
-	int cols;
-	
+typedef struct {
+	int map_y;	// vertical (y) map size
+	int map_x;	// horizontal (x) map size
+	char **arr2d;  // map data from file, w/o header, treated as const, solution bsq not written in it
+
 	char empty;
-	char full;
 	char obst;
-	
-	char **map;
-	FILE *stream;
-} 	t_bsq;
+	char full;
+
+	int sol_side;	// square size (side a) of the solution square
+	int sol_y;  // y coordinate, top-left corner as starting point of the solution square
+	int sol_x;  // x coordinate, top-left corner as starting point of the solution square
+} t_map;
 
 #endif
-
