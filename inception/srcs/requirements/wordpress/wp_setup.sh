911ce617b41d7087c66550883c1106423a31fa7f
jasnguye <jasminnguyen001@web.de>
2025-12-23 13:42:32 +0100

    can display a blog now

--- inception/srcs/requirements/wordpress/wp_setup.sh
+++ inception/srcs/requirements/wordpress/wp_setup.sh
@@ -2,8 +2,30 @@
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpassword --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin-email=admin@admin.com --allow-root
+# 2) avoid failing if WP files are already there 
+if [ ! -f wp-settings.php ]; then
+  ./wp-cli.phar core download --allow-root
+fi
+if [ ! -f wp-config.php ]; then
+  ./wp-cli.phar config create \
+    --dbname=wordpress \
+    --dbuser=wpuser \
+    --dbpass=password \
+    --dbhost=mariadb \
+    --allow-root
+fi
+# 4) fix admin email flag name: --admin_email (underscore)
+#    also: don’t reinstall if already installed
+if ! ./wp-cli.phar core is-installed --allow-root; then
+  ./wp-cli.phar core install \
+    --url=localhost:8080 \
+    --title=inception \
+    --admin_user=admin \
+    --admin_password=admin \
+    --admin_email=admin@admin.com \
+    --allow-root
+fi
 
-php-fpm8.2 -F
+# 5) use exec so php-fpm becomes PID 1
+exec php-fpm8.2 -F
+
